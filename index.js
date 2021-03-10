ae27ddffd0ea67198a392a8fa3a805bd139aae34
gabriel-demes <Gabrieldemes@nyu.edu>
2021-03-10 11:42:06 -0500

    basic functionality completed

--- index.js
+++ index.js
@@ -8,14 +8,23 @@ const editTaskDiv = taskDiv.querySelector(`div#edit-task-div`)
 const editTaskForm = taskDiv.querySelector(`form#edit-task-form`)
 const newTaskDiv = document.querySelector(`div#new-task-div`)
 const newTaskForm = document.querySelector(`form#new-task-form`)
-
+const newListForm = document.querySelector('form#new-list-form')
+const newListDiv = document.querySelector('div#new-list-div')
+const newListBtn = document.querySelector('button#new-list-btn')
+const hideListDiv = document.querySelector('#new-list-div')
+const hideNewTaskDiv = document.querySelector('#new-task-div')
 
 
 const loadUser = id =>{
     fetch(`${url}/users/${id}`)
         .then(resp => resp.json())
-        .then(user => populatePage(user))
+        .then(user => {populatePage(user)
+        newOrgForm.dataset.id = user.id})
+
     loadOrgForm()
+    newList()
+    contentContainerEvents()
+
 }
 
 const populatePage = userObj =>{
@@ -46,6 +55,9 @@ const clickOrg = li => {
 }
 
 const displayOrg = org => {
+    newListForm.dataset.id = org.id
+    newListBtn.style = "display:block"
+    newListBtn.dataset.id = org.id
     contentContainer.querySelectorAll('*').forEach(n => n.remove());
     if(org.lists.length >= 1){
         org.lists.forEach(list => {contentContainer.append(showList(list))})
@@ -56,25 +68,82 @@ const showList = list =>{
     const div = document.createElement('div')
         div.dataset.id = list.id
         div.classList.add('list')
-        div.innerHTML = `<div>
+        div.innerHTML = `<div class="listul">
                     <h3>${list.title}</h3>
-                    <ul></ul>
+                    <ul data-id="${list.id}"></ul>
                 </div>`
-        list.taskcards.forEach(task => addTaskCard(task, div))
+        if (list.taskcards){
+            list.taskcards.forEach(task => addTaskCard(task, div))
+            }
+
         const createTaskBtn = document.createElement('button')
-        newTask(createTaskBtn, div)
-        const ul = div.querySelector('ul').parentElement.append(createTaskBtn)
-        
-        
-        
+        createTaskBtn.dataset.id = list.id
+        createTaskBtn.id = "create-task-btn"
+        createTaskBtn.textContent = "Add Task"
+        const ul = div.querySelector('ul').parentElement
+        ul.append(createTaskBtn)
+        const deleteListBtn = document.createElement('button')
+        deleteListBtn.dataset.id = list.id
+        deleteListBtn.id = "delete-list-btn"
+        deleteListBtn.textContent = "Delete List"
+        ul.append(deleteListBtn)
     return div
+}
+
+const contentContainerEvents = () => {
+    contentContainer.addEventListener('click', function(event){
+        switch (event.target.id){
+            case "create-task-btn":
+                newTaskDiv.style="display:block"
+                hideNewTask()
+                newTaskForm.dataset.id = event.target.dataset.id
+                break
+            case "delete-list-btn":
+                deleteList(event.target.dataset.id)
+                break
+
+        }
+    })
+    newTaskForm.addEventListener('submit', function(event){
+        event.preventDefault()
+        const title =  event.target[0].value
+        const deadline = event.target[1].value
+        const description = event.target[2].value
+        const task = {title, deadline, description, list_id: newTaskForm.dataset.id}
+        createTask(task)
+        
+        })
+    
+    contentContainer.addEventListener('dragstart', function(event){
+        if (event.target.classList[0] === 'taskcard'){
+            event.target.classList.add('dragging')
+        }
+    })
+    contentContainer.addEventListener('dragend', function(event){
+        if (event.target.classList[1] === 'dragging'){
+            event.target.classList.remove('dragging')
+            handleMovement(event.target)
+        }
+    })
+    contentContainer.addEventListener("dragover", function(event){
+        if(event.target.classList[0]==="listul"){
+            event.preventDefault()
+            const taskcard = document.querySelector(".dragging")
+            const ul = event.target.querySelector('ul')
+            ul.append(taskcard)
+        }
+    })
 }
 
 const addTaskCard = (task, div) => {
     const card = document.createElement('li')
+            card.classList.add('taskcard')
+            card.draggable ="true"
             card.dataset.id = task.id
+            card.dataset.list = div.dataset.id
             card.innerHTML = `<h4>${task.title}</h4>`
-            const ul = div.querySelector('ul').append(card)
+            const ul = div.querySelector('ul')
+            ul.append(card)
             clickTask(card)
             
 }
@@ -116,13 +185,29 @@ const hideTask = () =>{
         }
     })
 }
+const hideNewList = () =>{
+    hideListDiv.addEventListener('click', function(event){
+        if(event.target.dataset.action === "close"){
+            hideListDiv.style="display:none"
+        }
+    })
+}
+
+const hideNewTask = () =>{
+    hideNewTaskDiv.addEventListener('click', function(event){
+        if(event.target.dataset.action === "close"){
+            hideNewTaskDiv.style="display:none"
+        }
+    })
+}
 
 const loadOrgForm = () => {
     const bttn = document.querySelector(`#new-org`)
+    submitNewOrgForm()
     bttn.addEventListener('click', function(event){
         hideNewOrgDiv.style ="display:block"
         hideNewOrgForm()
-        submitNewOrgForm()
+        
     })
 }
 
@@ -138,7 +223,8 @@ const submitNewOrgForm = () => {
     newOrgForm.addEventListener('submit', function(e){
         event.preventDefault()
         const name = event.target[0].value
-        const newOrg = {name}
+        const user_id = event.target.dataset.id
+        const newOrg = {name, user_id}
         createOrg(newOrg)
     })
 }
@@ -157,7 +243,7 @@ const joinOrg = org =>{
     const organization_id = org.id
     const user_id = 1
     const membership = {organization_id, user_id}
-    fetch(`${url}/memberships`,{
+    fetch(`${url}/memeberships`,{
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(membership)
@@ -215,26 +301,7 @@ const deleteTask = () =>{
     })
 }
 
-const newTask = (button, div) => {
-        button.textContent= "Add Task"
-        submitNewTask(div)
-        button.addEventListener('click', function(event){
-            newTaskDiv.style="display:block"})
-}
-
-const submitNewTask = div => {
-    newTaskForm.addEventListener('submit', function(event){
-        event.preventDefault()
-        const title =  event.target[0].value
-        const deadline = event.target[1].value
-        const description = event.target[2].value
-        const task = {title, deadline, description, list_id: div.dataset.id}
-        createTask(task, div)
-    })
-}
-
-const createTask = (task, div) => {
-    console.log("3")
+const createTask = (task) => {
     fetch(`${url}/taskcards`,{
         method: "POST",
         headers: {"Content-Type": "application/json"},
@@ -243,9 +310,68 @@ const createTask = (task, div) => {
     .then(resp => resp.json())
     .then(newTask => {
         displayTaskInfo(newTask)
-        addTaskCard(newTask, div)
+        addTaskCard(newTask, document.querySelector(`div[data-id="${newTask.list_id}"]`))
+        hideTaskDiv.style = "display:block"
         newTaskDiv.style = "display:none"
     })
+}
+
+const deleteList = id =>{
+    fetch(`${url}/lists/${id}`,{
+        method: "DELETE"
+    })
+    .then(resp => resp.json())
+    .then(list => {
+        document.querySelector(`div[data-id="${id}"]`).remove()
+    })
+}
+
+const newList = () =>{
+    submitNewList()
+    newListBtn.addEventListener('click', function(){
+        newListDiv.style="display:block"
+        hideNewList()
+    })
+}
+
+const submitNewList = () =>{
+    newListForm.addEventListener('submit', function(event){
+        event.preventDefault()
+        const title = event.target[0].value
+        const organization_id = event.target.dataset.id
+        const list = {title, organization_id}
+        createList(list)
+    })
+}
+
+const createList = (list) => {
+    let y = {}
+    fetch(`${url}/lists`,{
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(list)
+    })
+    .then(resp => resp.json())
+    .then(newList => {
+        contentContainer.append(showList(newList))
+        newListDiv.style = "display:none"
+        Object.assign(y, newList)
+    })
+    return console.log(y)
+}
+
+const handleMovement = card =>{
+    let currList = card.dataset.list
+    if(currList != card.parentElement.dataset.id){
+        currList = card.parentElement.dataset.id
+        fetch(`${url}/taskcards/${card.dataset.id}`,{
+            method: "PATCH",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({list_id: currList})
+        })
+        .then(resp => resp.json())
+        .then(task => {card.dataset.list = task.user_list})
+    }
 }
 
 loadUser(1)
